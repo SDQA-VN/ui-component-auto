dd2a0811205c78c1a516bde1dc8df0d14c8c3de3
Luc Tran Vinh Khiem <khiemltv@gmail.com>
2019-11-28 09:53:59 +0700

    roster update

--- test/testScripts/common/_uploadJSFiles.js
+++ test/testScripts/common/_uploadJSFiles.js
@@ -1,14 +1,15 @@
 const JsCustomization = require('../../pageObject/app/JsCssCustomization');
 const Common = require('../../utils/Common');
-const FileHandling = require('../../utils/FileHandling');
-const path = require('path')
-
-const KINTONE_JS_SDK_FILE_PATH = path.join(__dirname, '..', '..','..', 'node_modules', '@kintone', 'kintone-ui-component', 'dist', 'kintone-ui-component.min.js');
+const AppSettings = require('../../pageObject/app/AppSettings')
 
 describe('kintoneUIComponent - JS test - Upload files', function () {
     it('upload file', function () {
         Common.logInSlash();
-        Common.uploadFile();
+        JsCustomization.navigate();
+        JsCustomization.deleteAllJSFiles();
+        JsCustomization.uploadFile();
+        JsCustomization.save();
+        AppSettings.updateApp();
         Common.logOutSlash();
     })
 });
