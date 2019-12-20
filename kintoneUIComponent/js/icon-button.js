2812c8a24198db02f302f3e1d8e9516b6b4e7c09
Luc Tran Vinh Khiem <khiemltv@gmail.com>
2019-12-21 00:44:03 +0700

    update

--- kintoneUIComponent/js/icon-button.js
+++ kintoneUIComponent/js/icon-button.js
@@ -2,89 +2,211 @@
 // //https://sharedoc.atlassian.net/wiki/spaces/SC/pages/86737727/kintone-ui-component+IconButton
 // 'use strict';
 
-//let body = document.getElementsByTagName("BODY")[0];
-
 // Render() - full option value
 let cons_full_value = new kintoneUIComponent.IconButton({
     type: 'insert',
-    color:'blue', 
+    color: 'blue',
     size: 'small',
     isDisabled: false,
     isVisible: true
 });
-let cons_full_value_El = cons_full_value.render();
-cons_full_value_El.id = 'Full_Value_Icon_ID';
-let cons_full_value_cell = testgrid.getCell(posConfig.Icon_Button.x, 1);
-cons_full_value_cell.appendChild(cons_full_value_El);
+let iconFullOptionEl = document.createElement('div')
+iconFullOptionEl.setAttribute("id", "icon-full-options");
+iconFullOptionEl.appendChild(cons_full_value.render())
+let iconFullOptionCell = testgrid.getCell(posConfig.Icon_Button.x, 1);
+iconFullOptionCell.appendChild(iconFullOptionEl);
+
 
 // only option.type = insert
 let cons_type_insert = new kintoneUIComponent.IconButton({
     type: 'insert'
 });
-let cons_type_insert_El = cons_type_insert.render();
-cons_type_insert_El.id = 'Type_Insert_Icon_ID';
-let cons_type_insert_cell = testgrid.getCell(posConfig.Icon_Button.x, 2);
-cons_type_insert_cell.appendChild(cons_type_insert_El);
+let iconTypeInsertEl = document.createElement('div')
+iconTypeInsertEl.setAttribute("id", "icon-only-type");
+iconTypeInsertEl.appendChild(cons_type_insert.render())
+let iconTypeInsertCell = testgrid.getCell(posConfig.Icon_Button.x, 2);
+iconTypeInsertCell.appendChild(iconTypeInsertEl);
+
 
 // only option.color = green
 let cons_color_green = new kintoneUIComponent.IconButton({
     color: 'green'
 });
-let cons_color_green_El = cons_color_green.render();
-cons_color_green_El.id = 'Color_Green_Icon_ID';
-let cons_color_green_cell = testgrid.getCell(posConfig.Icon_Button.x, 3);
-cons_color_green_cell.appendChild(cons_color_green_El);
+let iconColorGreenEl = document.createElement('div')
+iconColorGreenEl.setAttribute("id", "icon-only-color");
+iconColorGreenEl.appendChild(cons_color_green.render())
+let iconColorGreenCell = testgrid.getCell(posConfig.Icon_Button.x, 3);
+iconColorGreenCell.appendChild(iconColorGreenEl);
+
 
 // only option.isDisabled = true
 let cons_disabled = new kintoneUIComponent.IconButton({
     isDisabled: true
 });
-let cons_disabled_El = cons_disabled.render();
-cons_disabled_El.id = 'Disabled_Icon_ID';
-let cons_disabled_cell = testgrid.getCell(posConfig.Icon_Button.x, 4);
-cons_disabled_cell.appendChild(cons_disabled_El);
+let iconDisabledEl = document.createElement('div')
+iconDisabledEl.setAttribute("id", "icon-disabled");
+// set disable
+let buttonDisable = document.createElement('button')
+buttonDisable.innerHTML = 'Disable Icon'
+buttonDisable.addEventListener('click', function () {
+    cons_disabled.disable();
+})
+// set enable
+let buttonEnable = document.createElement('button')
+buttonEnable.innerHTML = 'Enable Icon'
+buttonEnable.addEventListener('click', function () {
+    cons_disabled.enable();
+})
+// show
+let buttonShow = document.createElement('button')
+buttonShow.innerHTML = 'Show Icon'
+buttonShow.addEventListener('click', function () {
+    cons_disabled.show();
+})
+// hide
+let buttonHide = document.createElement('button')
+buttonHide.innerHTML = 'Hide Icon'
+buttonHide.addEventListener('click', function () {
+    cons_disabled.hide();
+})
+iconDisabledEl.appendChild(cons_disabled.render())
+iconDisabledEl.appendChild(buttonDisable)
+iconDisabledEl.appendChild(buttonEnable)
+iconDisabledEl.appendChild(buttonShow)
+iconDisabledEl.appendChild(buttonHide)
+let iconDisabledCell = testgrid.getCell(posConfig.Icon_Button.x, 4);
+iconDisabledCell.appendChild(iconDisabledEl);
+
 
 // setType
 let setTypeForIB = new kintoneUIComponent.IconButton({
     isDisabled: false
 });
-let setTypeForIB_El = setTypeForIB.render();
-setTypeForIB_El.id = 'setType_Icon_ID';
-let setTypeForIB_cell = testgrid.getCell(posConfig.Icon_Button.x, 5);
-setTypeForIB_cell.appendChild(setTypeForIB_El);
+let iconSetTypeEl = document.createElement('div')
+iconSetTypeEl.setAttribute("id", "icon-set-type");
+// set type insert
+let buttonSetTypeInsert = document.createElement('button')
+buttonSetTypeInsert.innerHTML = 'Set Type Insert'
+buttonSetTypeInsert.addEventListener('click', function () {
+    setTypeForIB.setType('insert');
+})
+// set type remove
+let buttonSetTypeRemove = document.createElement('button')
+buttonSetTypeRemove.innerHTML = 'Set Type Remove'
+buttonSetTypeRemove.addEventListener('click', function () {
+    setTypeForIB.setType('remove');
+})
+// set type close
+let buttonSetTypeClose = document.createElement('button')
+buttonSetTypeClose.innerHTML = 'Set Type Close'
+buttonSetTypeClose.addEventListener('click', function () {
+    setTypeForIB.setType('close');
+})
+// set type file
+let buttonSetTypeFile = document.createElement('button')
+buttonSetTypeFile.innerHTML = 'Set Type File'
+buttonSetTypeFile.addEventListener('click', function () {
+    setTypeForIB.setType('file');
+})
+// set type right
+let buttonSetTypeRight = document.createElement('button')
+buttonSetTypeRight.innerHTML = 'Set Type Right'
+buttonSetTypeRight.addEventListener('click', function () {
+    setTypeForIB.setType('right');
+})
+// set type left
+let buttonSetTypeLeft = document.createElement('button')
+buttonSetTypeLeft.innerHTML = 'Set Type Left'
+buttonSetTypeLeft.addEventListener('click', function () {
+    setTypeForIB.setType('left');
+})
+// get icon type state
+let buttonGetStateType = document.createElement('button')
+buttonGetStateType.innerHTML = 'Get State Type'
+buttonGetStateType.addEventListener('click', function () {
+    let stateOFIB = setTypeForIB._props.type;
+    alert(stateOFIB);
+})
 
-function getStateOfIB() {
-    let stateOFIB = setTypeForIB._props;
-    alert(JSON.stringify(stateOFIB));
-}
+iconSetTypeEl.appendChild(setTypeForIB.render())
+iconSetTypeEl.appendChild(buttonSetTypeInsert)
+iconSetTypeEl.appendChild(buttonSetTypeRemove)
+iconSetTypeEl.appendChild(buttonSetTypeClose)
+iconSetTypeEl.appendChild(buttonSetTypeFile)
+iconSetTypeEl.appendChild(buttonSetTypeRight)
+iconSetTypeEl.appendChild(buttonSetTypeLeft)
+iconSetTypeEl.appendChild(buttonGetStateType)
+let iconSetTypeCell = testgrid.getCell(posConfig.Icon_Button.x, 5);
+iconSetTypeCell.appendChild(iconSetTypeEl);
 
-let getValueIBBtn = new kintoneUIComponent.Button({
-    text: 'Get State For Icon Button',
-    type: 'normal',
-    isVisible: true,
-});
 
-setTypeForIB_cell.appendChild(getValueIBBtn.render());
-getValueIBBtn.on('click', getStateOfIB);
-
-setTypeForIB.on('click', function (event) {
-    alert('on_click has been clicked');
-});
-
-// on trigger
-let on_trigger = new kintoneUIComponent.IconButton({
+// set size
+let icon_set_size = new kintoneUIComponent.IconButton({
     type: 'insert',
     isDisabled: false,
     isVisible: true
 });
-let on_trigger_IB_El = on_trigger.render();
-on_trigger_IB_El.id = 'on_trigger_Icon_ID';
-let on_trigger_cell = testgrid.getCell(posConfig.Icon_Button.x, 6);
-on_trigger_cell.appendChild(on_trigger_IB_El);
+// set size normal
+let buttonSetSizeNormal = document.createElement('button')
+buttonSetSizeNormal.innerHTML = 'Set Size Normal'
+buttonSetSizeNormal.addEventListener('click', function () {
+    icon_set_size.setSize('normal');
+})
+// set size small
+let buttonSetSizeSmall = document.createElement('button')
+buttonSetSizeSmall.innerHTML = 'Set Size Small'
+buttonSetSizeSmall.addEventListener('click', function () {
+    icon_set_size.setSize('small');
+})
+let iconSetSizeEl = document.createElement('div')
+iconSetSizeEl.setAttribute("id", "icon-set-size");
+iconSetSizeEl.appendChild(icon_set_size.render());
+iconSetSizeEl.appendChild(buttonSetSizeNormal);
+iconSetSizeEl.appendChild(buttonSetSizeSmall);
+let iconSetSizeCell = testgrid.getCell(posConfig.Icon_Button.x, 6);
+iconSetSizeCell.appendChild(iconSetSizeEl);
 
-function on_trigger_IB() {
+// set shape
+let icon_set_shape = new kintoneUIComponent.IconButton({
+    type: 'insert',
+    isDisabled: false,
+    isVisible: true
+});
+// set shape circle
+let buttonSetShapeCirle = document.createElement('button')
+buttonSetShapeCirle.innerHTML = 'Set Shape Circle'
+buttonSetShapeCirle.addEventListener('click', function () {
+    icon_set_shape.setShape('circle');
+})
+// set shape normal
+let buttonSetShapeNormal = document.createElement('button')
+buttonSetShapeNormal.innerHTML = 'Set Shape Normal'
+buttonSetShapeNormal.addEventListener('click', function () {
+    icon_set_shape.setShape('normal');
+})
+let iconSetShapeEl = document.createElement('div')
+iconSetShapeEl.setAttribute("id", "icon-set-shape");
+iconSetShapeEl.appendChild(icon_set_shape.render());
+iconSetShapeEl.appendChild(buttonSetShapeNormal);
+iconSetShapeEl.appendChild(buttonSetShapeCirle);
+let iconSetShapeCell = testgrid.getCell(posConfig.Icon_Button.x, 7);
+iconSetShapeCell.appendChild(iconSetShapeEl);
+
+
+// on trigger
+let icon_on_trigger = new kintoneUIComponent.IconButton({
+    type: 'insert',
+    isDisabled: false,
+    isVisible: true
+});
+
+let iconOnTriggerEl = document.createElement('div')
+iconOnTriggerEl.setAttribute("id", "icon-on-trigger");
+iconOnTriggerEl.appendChild(icon_on_trigger.render());
+let iconOnTriggerCell = testgrid.getCell(posConfig.Icon_Button.x, 8);
+iconOnTriggerCell.appendChild(iconOnTriggerEl);
+icon_on_trigger.on('click', function(){
     alert('on_click_trigger has been clicked');
-}
-on_trigger.on('click', on_trigger_IB);
+});
 
 
--- test/testScripts/js-test/icon-button.js
+++ test/testScripts/js-test/icon-button.js
@@ -1,9 +1,33 @@
-const $ = require('../../utils/ReturnElement').singleElement;
-const isVisibled = require('../../utils/ReturnElement').waitUntilSelectorVisibled;
-const isEnabled = require('../../utils/ReturnElement').waitUntilSelectorEnabled;
 const common = require('../../utils/common');
-const expect = require('chai').expect;
-const xpath = require('../../xpath/js-test/icon-button.xpath');
+const Helper = require('../../helper/main');
+
+const ICON_FULL_OPTIONS = "//div[@id='icon-full-options']//button[contains(@class,'kuc-icon-btn')]";
+const ICON_ONLY_TYPE = "//div[@id='icon-only-type']/button[contains(@class,'kuc-icon-btn')]";
+const ICON_ONLY_COLOR = "//div[@id='icon-only-color']/button[contains(@class,'kuc-icon-btn')]";
+
+const ICON_DISABLED = "//div[@id='icon-disabled']/button[contains(@class,'kuc-icon-btn')]";
+const ICON_SHOW_BUTTON = "//button[text()='Show Icon']";
+const ICON_HIDE_BUTTON = "//button[text()='Hide Icon']";
+const ICON_DISABLE_BUTTON = "//button[text()='Disable Icon']";
+const ICON_ENABLE_BUTTON = "//button[text()='Enable Icon']";
+
+const ICON_SET_TYPE_INSERT_BUTTON = "//button[text()='Set Type Insert']";
+const ICON_SET_TYPE_REMOVE_BUTTON = "//button[text()='Set Type Remove']";
+const ICON_SET_TYPE_CLOSE_BUTTON = "//button[text()='Set Type Close']";
+const ICON_SET_TYPE_FILE_BUTTON = "//button[text()='Set Type File']";
+const ICON_SET_TYPE_RIGHT_BUTTON = "//button[text()='Set Type Right']";
+const ICON_SET_TYPE_LEFT_BUTTON = "//button[text()='Set Type Left']";
+const ICON_SET_TYPE_GET_STATE_BUTTON = "//button[text()='Get State Type']";
+
+const ICON_SIZE = "//div[@id='icon-set-size']/button[contains(@class,'kuc-icon-btn')]";
+const ICON_SET_SIZE_NORMAL_BUTTON = "//button[text()='Set Size Normal']";
+const ICON_SET_SIZE_SMALL_BUTTON = "//button[text()='Set Size Small']";
+
+const ICON_SHAPE = "//div[@id='icon-set-shape']/button[contains(@class,'kuc-icon-btn')]";
+const ICON_SET_SHAPE_NORMAL_BUTTON = "//button[text()='Set Shape Normal']";
+const ICON_SET_SHAPE_CIRCLE_BUTTON = "//button[text()='Set Shape Circle']";
+
+const ICON_ON_TRIGGER = "//div[@id='icon-on-trigger']/button[contains(@class,'kuc-icon-btn')]";
 
 describe('kintoneUIComponent - Icon button', function () {
     before(() => {
@@ -14,129 +38,95 @@ describe('kintoneUIComponent - Icon button', function () {
     });
 
     it('[Icon-Button-2] should verify UI', function () {
-        let iconButtonBgColor = $(xpath.XPATH_FULL_OPTIONS_VALUE).getCssProperty('color');
-        let iconButtonSize = browser.getElementSize(xpath.XPATH_FULL_OPTIONS_VALUE);
-        expect(iconButtonBgColor.parsed.hex).to.equal('#333333');
-        expect(iconButtonSize.width).to.equal(iconButtonSize.width);
-        expect(iconButtonSize.height).to.equal(iconButtonSize.height);
+        Helper.ElementHandler
+            .waitForElement(ICON_FULL_OPTIONS)
+            .verifyElementColor(ICON_FULL_OPTIONS, 'color', '#333333')
+            .verifyElementSize(ICON_FULL_OPTIONS, 16, 16)
     });
 
     it('[Icon-Button-3-5-6-14-15] should create a Icon Button', function () {
-        let isVisibleItem = isVisibled(xpath.XPATH_FULL_OPTIONS_VALUE, true);
-        let isVisibleOnlyType = isVisibled(xpath.XPATH_ONLY_TYPE, true);
-        let isVisibleOnlyColor = isVisibled(xpath.XPATH_ONLY_COLOR, true);
-        let isVisibleDisabled = isVisibled(xpath.XPATH_DISABLED, true);
-        expect(isVisibleItem).to.equal(true);
-        expect(isVisibleOnlyType).to.equal(true);
-        expect(isVisibleOnlyColor).to.equal(true);
-        expect(isVisibleDisabled).to.equal(true);
+        Helper.ElementHandler
+            .waitForElement(ICON_FULL_OPTIONS)
+            .verifyElementVisible(ICON_FULL_OPTIONS)
+            .verifyElementVisible(ICON_ONLY_TYPE)
+            .verifyElementVisible(ICON_ONLY_COLOR)
+            .verifyElementVisible(ICON_DISABLED)
     });
 
     it('[Icon-Button-20-21-22-26] should set type for icon button', function () {
-        $(xpath.XPATH_BODY).execute(() => {
-            setTypeForIB.setType('insert');
-        });
-        $(xpath.XPATH_GET_STATE_BTN).click();
-        let alertText = browser.alertText();
-        expect(alertText).to.include('"type":"insert"');
-        browser.alertAccept();
+        Helper.ElementHandler
+            .click(ICON_SET_TYPE_INSERT_BUTTON)
+            .click(ICON_SET_TYPE_GET_STATE_BUTTON)
+            .verifyAlertText('insert')
 
-        $(xpath.XPATH_BODY).execute(() => {
-            setTypeForIB.setType('remove');
-        });
-        $(xpath.XPATH_GET_STATE_BTN).click();
-        alertText = browser.alertText();
-        expect(alertText).to.include('"type":"remove"');
-        browser.alertAccept();
+            .click(ICON_SET_TYPE_REMOVE_BUTTON)
+            .click(ICON_SET_TYPE_GET_STATE_BUTTON)
+            .verifyAlertText('remove')
 
-        $(xpath.XPATH_BODY).execute(() => {
-            setTypeForIB.setType('close');
-        });
-        $(xpath.XPATH_GET_STATE_BTN).click();
-        alertText = browser.alertText();
-        expect(alertText).to.include('"type":"close"');
-        browser.alertAccept();
+            .click(ICON_SET_TYPE_CLOSE_BUTTON)
+            .click(ICON_SET_TYPE_GET_STATE_BUTTON)
+            .verifyAlertText('close')
 
-        $(xpath.XPATH_BODY).execute(() => {
-            setTypeForIB.hide();
-        });
+            .click(ICON_SET_TYPE_FILE_BUTTON)
+            .click(ICON_SET_TYPE_GET_STATE_BUTTON)
+            .verifyAlertText('file')
 
-        $(xpath.XPATH_BODY).execute(() => {
-            setTypeForIB.setType('insert');
-        });
-        $(xpath.XPATH_GET_STATE_BTN).click();
-        alertText = browser.alertText();
-        expect(alertText).to.include('"type":"insert"');
-        expect(alertText).to.include('"isVisible":false');
-        browser.alertAccept();
+            .click(ICON_SET_TYPE_RIGHT_BUTTON)
+            .click(ICON_SET_TYPE_GET_STATE_BUTTON)
+            .verifyAlertText('right')
 
-        $(xpath.XPATH_BODY).execute(() => {
-            setTypeForIB.show();
-        });
+            .click(ICON_SET_TYPE_LEFT_BUTTON)
+            .click(ICON_SET_TYPE_GET_STATE_BUTTON)
+            .verifyAlertText('left')
     });
 
     it('[Icon-Button-37-38] should set size for icon button', function () {
-        $(xpath.XPATH_BODY).execute(() => {
-            setTypeForIB.setSize('large');
-        });
-        $(xpath.XPATH_GET_STATE_BTN).click();
-        let alertText = browser.alertText();
-        expect(alertText).to.include('"size":"large"');
-        browser.alertAccept();
+        Helper.ElementHandler
+            .waitForElement(ICON_SIZE)
+            .click(ICON_SET_SIZE_SMALL_BUTTON)
+            .verifyAttribute(ICON_SIZE, 'class', 'small')
 
-        $(xpath.XPATH_BODY).execute(() => {
-            setTypeForIB.setSize('small');
-        });
-        $(xpath.XPATH_GET_STATE_BTN).click();
-        alertText = browser.alertText();
-        expect(alertText).to.include('"size":"small"');
-        browser.alertAccept();
+            .click(ICON_SET_SIZE_NORMAL_BUTTON)
+            .verifyAttribute(ICON_SIZE, 'class', 'large')
+    });
+
+    it('[Icon-Button-37-38] should set shape for icon button', function () {
+        Helper.ElementHandler
+            .waitForElement(ICON_SHAPE)
+            .click(ICON_SET_SHAPE_CIRCLE_BUTTON)
+            .verifyAttribute(ICON_SHAPE, 'class', 'circle')
+
+            .click(ICON_SET_SHAPE_NORMAL_BUTTON)
+            .verifyAttribute(ICON_SHAPE, 'class', 'normal')
     });
 
     it('[Icon-Button-45] should hide visible Icon button on UI', function () {
-        $(xpath.XPATH_BODY).execute(() => {
-            setTypeForIB.hide();
-        });
-        let isVisibleItem = isVisibled(xpath.XPATH_SET_TYPE_ICON_BUTTON, false);
-        expect(isVisibleItem).to.equal(false);
+        Helper.ElementHandler
+            .click(ICON_HIDE_BUTTON)
+            .verifyElementNotVisible(ICON_DISABLED)
     });
 
     it('[Icon-Button-43] should show invisible Icon button on UI', function () {
-        $(xpath.XPATH_BODY).execute(() => {
-            setTypeForIB.show();
-        });
-        let isVisibleItem = isVisibled(xpath.XPATH_SET_TYPE_ICON_BUTTON, true);
-        expect(isVisibleItem).to.equal(true);
+        Helper.ElementHandler
+            .click(ICON_SHOW_BUTTON)
+            .verifyElementVisible(ICON_DISABLED)
     });
 
     it('[Icon-Button-47] should disable Icon button on UI', function () {
-        $(xpath.XPATH_BODY).execute(() => {
-            setTypeForIB.disable();
-        });
-        let isEnableItem = isEnabled(xpath.XPATH_SET_TYPE_IB_DISABLED, false);
-        expect(isEnableItem).to.equal(false);
+        Helper.ElementHandler
+            .click(ICON_DISABLE_BUTTON)
+            .verifyElementDisabled(ICON_DISABLED)
     });
 
     it('[Icon-Button-50] should enable Icon button on UI', function () {
-        $(xpath.XPATH_BODY).execute(() => {
-            setTypeForIB.enable();
-        });
-        let isEnableItem = isEnabled(xpath.XPATH_SET_TYPE_IB_DISABLED, true);
-        expect(isEnableItem).to.equal(true);
+        Helper.ElementHandler
+            .click(ICON_ENABLE_BUTTON)
+            .verifyElementEnabled(ICON_DISABLED)
     });
 
-    it('[Icon-button-51] should callback function', function () {
-        $(xpath.XPATH_SET_TYPE_ICON_BUTTON).click();
-        let alertText = browser.alertText();
-        expect(alertText).to.equal('on_click has been clicked');
-        browser.alertAccept();
+    it('[Icon-button-51-52] should callback trigger function', function () {
+        Helper.ElementHandler
+            .click(ICON_ON_TRIGGER)
+            .verifyAlertText('on_click_trigger has been clicked')
     });
-
-    it('[Icon-button-52] should callback trigger function', function () {
-        $(xpath.XPATH_ON_TRIGGER_IB).click();
-        let alertText = browser.alertText();
-        expect(alertText).to.equal('on_click_trigger has been clicked');
-        browser.alertAccept();
-    });
-    
 });
