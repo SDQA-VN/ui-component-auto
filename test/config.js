f3501a2b945c02793cc2f1833ce964eb18342bef
Luc Tran Vinh Khiem <khiemltv@gmail.com>
2020-01-02 14:04:03 +0700

    update

--- test/config.js
+++ test/config.js
@@ -1,5 +1,5 @@
 module.exports = {	
-    "domain": process.env.testtt,	 
+    "domain": process.env.domain,	 
     "appId": process.env.appID,	  
     "credentials": {
         "username": process.env.username,	
