71ac1737af72f1c9a07f7c39240317aad0a2bd2d
Luc Tran Vinh Khiem <khiemltv@gmail.com>
2020-01-15 17:35:45 +0700

    update

--- actions/get_latest_build/index.js
+++ actions/get_latest_build/index.js
@@ -1,10 +1,10 @@
 const execSync = require("child_process").execSync;
 const core = require("@actions/core");
 const path = require('path');
-const fs = require('fs'); 
+const fs = require('fs');
 
-const sourePath = path.join(__dirname, 'kintone-ui-component');
-const packagePath = path.join(__dirname, 'kintone-ui-component', 'package.json');
+const sourePath = path.join(__dirname, '..', '..', 'kintone-ui-component',);
+const packagePath = path.join(__dirname, '..', '..', 'kintone-ui-component', 'package.json');
 
 const branch = core.getInput('branch')
 if (fs.existsSync(sourePath)) {
@@ -14,8 +14,9 @@ execSync(`git clone https://github.com/kintone/kintone-ui-component.git -b ${bra
 execSync(`cd ./kintone-ui-component && npm install`);
 execSync(`cd ./kintone-ui-component && npm run build`);
 execSync(`cd ./kintone-ui-component && npm pack`);
+execSync(`ls -l`);
 const package = require(packagePath)
-core.setOutput('files', path.join(__dirname, 'kintone-ui-component',`'kintone-kintone-ui-component-${package.version}.tgz`, ))
+core.setOutput('files', path.join(__dirname, '..', '..', 'kintone-ui-component', `'kintone-kintone-ui-component-${package.version}.tgz`))
 
 
 
