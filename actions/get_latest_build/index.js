39fb94a95da8be22b207f2cafaa68c35a1772af4
Luc Tran Vinh Khiem <khiemltv@gmail.com>
2020-01-15 17:22:01 +0700

    update

--- actions/get_latest_build/index.js
+++ actions/get_latest_build/index.js
@@ -4,6 +4,8 @@ const path = require('path');
 const fs = require('fs'); 
 
 const sourePath = path.join(__dirname, 'kintone-ui-component');
+const packagePath = path.join(__dirname, 'kintone-ui-component', 'package.json');
+
 const branch = core.getInput('branch')
 if (fs.existsSync(sourePath)) {
     execSync(`rm -rf ./kintone-ui-component`);
@@ -12,7 +14,7 @@ execSync(`git clone https://github.com/kintone/kintone-ui-component.git -b ${bra
 execSync(`cd ./kintone-ui-component && npm install`);
 execSync(`cd ./kintone-ui-component && npm run build`);
 execSync(`cd ./kintone-ui-component && npm pack`);
-const package = require('./kintone-ui-component/package.json')
+const package = require(packagePath)
 core.setOutput('files', path.join(__dirname, 'kintone-ui-component',`'kintone-kintone-ui-component-${package.version}.tgz`, ))
 
 
