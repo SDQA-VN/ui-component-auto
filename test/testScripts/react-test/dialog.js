7066815792294714f071df4aba3ad1e7bb556f7e
Luc Tran Vinh Khiem <khiemltv@gmail.com>
2020-03-02 10:58:12 +0700

    update

--- test/testScripts/react-test/dialog.js
+++ test/testScripts/react-test/dialog.js
@@ -3,14 +3,14 @@ const common = require('../../utils/Common.js');
 
 const SHOW_DIALOG_FULL_OPTION_BUTTON = '.dialog_full_option_button';
 const DIALOG_FULL_OPTION = '.dialog_full_optionEl .kuc-dialog-wrapper';
-const DIALOG_FULL_OPTION_CLOSE_BUTTON = '.dialog_full_optionEl .kuc-dialog-close-button .kuc-icon-btn svg';
+const DIALOG_FULL_OPTION_CLOSE_BUTTON = '.dialog_full_optionEl .kuc-dialog-close-button .kuc-icon-btn';
 
 const DIALOG_SET_HEADER_FOR_EXISTING_HEADER_BUTTON = '.dialog_setHeader_existing_header_button';
 const DIALOG_SET_HEADER_FOR_EXISTING_HEADER_VALUE = '.dialog_setHeader_existing_headerEl .kuc-dialog-header';
-const DIALOG_SET_HEADER_FOR_EXISTING_HEADER_CLOSE_BUTTON = '.dialog_setHeader_existing_headerEl .kuc-dialog-close-button .kuc-icon-btn svg';
+const DIALOG_SET_HEADER_FOR_EXISTING_HEADER_CLOSE_BUTTON = '.dialog_setHeader_existing_headerEl .kuc-dialog-close-button .kuc-icon-btn';
 
 const DIALOG_SET_HEADER_WITH_COMPONENTS_BUTTON = '.dialog_setHeader_with_components_button';
-const DIALOG_SET_HEADER_WITH_COMPONENTS_CLOSE_BUTTON = '.dialog_setHeader_with_componentsEl .kuc-dialog-close-button .kuc-icon-btn svg';
+const DIALOG_SET_HEADER_WITH_COMPONENTS_CLOSE_BUTTON = '.dialog_setHeader_with_componentsEl .kuc-dialog-close-button .kuc-icon-btn';
 const DIALOG_SET_HEADER_WITH_COMPONENTS_NORMAL_BUTTON = '.dialog_setHeader_with_componentsEl .kuc-btn.normal'
 const DIALOG_SET_HEADER_WITH_COMPONENTS_SUBMIT_BUTTON = '.dialog_setHeader_with_componentsEl .kuc-btn.submit'
 const DIALOG_SET_HEADER_WITH_COMPONENTS_TEXT_BOX = '.dialog_setHeader_with_componentsEl .dialog_set_header_with_components > .kuc-input-text'
@@ -40,10 +40,10 @@ const DIALOG_GET_HEADER_WITH_COMPONENTS_TABLE = '.dialog_get_header_react_elemen
 
 const DIALOG_SET_CONTENT_FOR_EXISTING_CONTENT_BUTTON = '.dialog_setContent_existing_content_button'
 const DIALOG_SET_CONTENT_FOR_EXISTING_CONTENT_VALUE = '.dialog_setContent_existing_contentEl .kuc-dialog-body'
-const DIALOG_SET_CONTENT_FOR_EXISTING_CONTENT_CLOSE_BUTTON = '.dialog_setContent_existing_contentEl .kuc-dialog-close-button .kuc-icon-btn svg'
+const DIALOG_SET_CONTENT_FOR_EXISTING_CONTENT_CLOSE_BUTTON = '.dialog_setContent_existing_contentEl .kuc-dialog-close-button .kuc-icon-btn '
 
 const DIALOG_SET_CONTENT_WITH_COMPONENTS_BUTTON = '.dialog_setContent_with_components_button'
-const DIALOG_SET_CONTENT_WITH_COMPONENTS_CLOSE_BUTTON = '.dialog_setContent_with_componentsEl .kuc-dialog-close-button .kuc-icon-btn svg';
+const DIALOG_SET_CONTENT_WITH_COMPONENTS_CLOSE_BUTTON = '.dialog_setContent_with_componentsEl .kuc-dialog-close-button .kuc-icon-btn ';
 const DIALOG_SET_CONTENT_WITH_COMPONENTS_NORMAL_BUTTON = '.dialog_setContent_with_componentsEl .kuc-btn.normal'
 const DIALOG_SET_CONTENT_WITH_COMPONENTS_SUBMIT_BUTTON = '.dialog_setContent_with_componentsEl .kuc-btn.submit'
 const DIALOG_SET_CONTENT_WITH_COMPONENTS_TEXT_BOX = '.dialog_setContent_with_componentsEl .dialog_set_content_with_components > .kuc-input-text'
@@ -73,10 +73,10 @@ const DIALOG_GET_CONTENT_WITH_COMPONENTS_TABLE = '.dialog_get_content_react_elem
 
 const DIALOG_SET_FOOTER_FOR_EXISTING_FOOTER_BUTTON = '.dialog_setFooter_existing_footer_button'
 const DIALOG_SET_FOOTER_FOR_EXISTING_FOOTER_VALUE = '.dialog_setFooter_existing_footerEl .kuc-dialog-footer'
-const DIALOG_SET_FOOTER_FOR_EXISTING_FOOTER_CLOSE_BUTTON = '.dialog_setFooter_existing_footerEl .kuc-dialog-close-button .kuc-icon-btn svg'
+const DIALOG_SET_FOOTER_FOR_EXISTING_FOOTER_CLOSE_BUTTON = '.dialog_setFooter_existing_footerEl .kuc-dialog-close-button .kuc-icon-btn '
 
 const DIALOG_SET_FOOTER_WITH_COMPONENTS_BUTTON = '.dialog_setFooter_with_components_button'
-const DIALOG_SET_FOOTER_WITH_COMPONENTS_CLOSE_BUTTON = '.dialog_setFooter_with_componentsEl .kuc-dialog-close-button .kuc-icon-btn svg';
+const DIALOG_SET_FOOTER_WITH_COMPONENTS_CLOSE_BUTTON = '.dialog_setFooter_with_componentsEl .kuc-dialog-close-button .kuc-icon-btn';
 const DIALOG_SET_FOOTER_WITH_COMPONENTS_NORMAL_BUTTON = '.dialog_setFooter_with_componentsEl .kuc-btn.normal'
 const DIALOG_SET_FOOTER_WITH_COMPONENTS_SUBMIT_BUTTON = '.dialog_setFooter_with_componentsEl .kuc-btn.submit'
 const DIALOG_SET_FOOTER_WITH_COMPONENTS_TEXT_BOX = '.dialog_setFooter_with_componentsEl .dialog_set_footer_with_components > .kuc-input-text'
@@ -106,17 +106,17 @@ const DIALOG_GET_FOOTER_WITH_COMPONENTS_TABLE = '.dialog_get_footer_react_elemen
 
 const SHOW_INVISIBLE_DIALOG_BUTTON = '.show_dialog_invisible_button'
 const DIALOG_INVISIBLE = '.dialog_invisibleEl .kuc-dialog-wrapper'
-const DIALOG_INVISIBLE_CLOSE_BUTTON = '.dialog_invisibleEl .kuc-dialog-close-button .kuc-icon-btn svg'
+const DIALOG_INVISIBLE_CLOSE_BUTTON = '.dialog_invisibleEl .kuc-dialog-close-button .kuc-icon-btn'
 
 const RENDER_VISIBLE_DIALOG_BUTTON = '.dialog_visibleEl .render_dialog_visible_button'
 const DIALOG_VISIBLE = '.dialog_visibleEl .kuc-dialog-wrapper'
 const HIDE_VISIBLE_DIALOG_BUTTON = '.dialog_visibleEl .hide_dialog_visible_button'
 
 const SHOW_DIALOG_WITH_CLOSE_BUTTON_BUTTON = '.dialog_show_closeButtonEl .dialog_show_closeButton_button'
-const DIALOG_SHOW_CLOSE_BUTTON = '.dialog_show_closeButtonEl .kuc-dialog-close-button .kuc-icon-btn svg'
+const DIALOG_SHOW_CLOSE_BUTTON = '.dialog_show_closeButtonEl .kuc-dialog-close-button .kuc-icon-btn'
 
 const SHOW_DIALOG_WITHOUT_CLOSE_BUTTON_BUTTON = '.dialog_hide_closeButtonEl .dialog_hide_closeButton_button'
-const DIALOG_HIDE_CLOSE_BUTTON = '.dialog_hide_closeButtonEl .kuc-dialog-close-button .kuc-icon-btn svg'
+const DIALOG_HIDE_CLOSE_BUTTON = '.dialog_hide_closeButtonEl .kuc-dialog-close-button .kuc-icon-btn'
 const CLOSE_DIALOG_WITHOUT_CLOSE_BUTTON_BUTTON = '.dialog_hide_closeButtonEl .close_dialog_without_closeButton_button'
 
 describe('kintoneUIComponent - Dialog', function () {
